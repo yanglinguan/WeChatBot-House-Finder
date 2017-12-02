0ee3ee0efa0db92c6222bd443438d254f4f63ba6
yanglinguan <yanglinguan@gmail.com>
2017-12-02 21:28:12 +0000

    config

--- src/run.sh
+++ src/run.sh
@@ -1,20 +1,20 @@
-
+export HOUSE_FINDER_HOME=$HOME/gitsrc/WeChatBot-House-Finder
 ## start webserver
-#cd $HOME/gitsrc/WeChatBot-House-Finder/src/web_server/client
-#npm install
-#npm run build
-#cd $HOME/gitsrc/WeChatBot-House-Finder/src/web_server/server
-#npm install
-#npm start &
+cd $HOUSE_FINDER_HOME/src/web_server/client
+npm install
+npm run build
+cd $HOUSE_FINDER_HOME/src/web_server/server
+npm install
+npm start &
 
 ## start backendserver
-cd $HOME/gitsrc/WeChatBot-House-Finder/src/backend_server
+cd $HOUSE_FINDER_HOME/src/backend_server
 python service.py &
 
 ## start wechatserver
-cd $HOME/gitsrc/WeChatBot-House-Finder/src/wechat_server
+cd $HOUSE_FINDER_HOME/src/wechat_server
 python wechat_server.py &
 
 ## start pipeline
-cd $HOME/gitsrc/WeChatBot-House-Finder/src/house_listing_pipeline
+cd $HOUSE_FINDER_HOME/src/house_listing_pipeline
 ./house_listing_pipeline_launcher.sh
